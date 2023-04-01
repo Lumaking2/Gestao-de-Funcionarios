6c2fa7dfb677cc1ad99de6a6a8ffa10540e4a697
Matheus Augusto <104650578+Lumaking2@users.noreply.github.com>
2023-04-01 14:41:33 -0300

    Implementação da Classe Funcionário

--- src/MODEL/Funcionario.java
+++ src/MODEL/Funcionario.java
@@ -1,5 +1,102 @@
 package MODEL;
 
-public class Funcionario {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+public class Funcionario implements Serializable {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private Date dataNascimento;
+    private Double salarioBase;
+
+    private Departamento departamento;
+
+    public Funcionario(){
+
+    }
+
+    public Funcionario(Integer id, String nome, String email, Date dataNascimento, Double salarioBase, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.salarioBase = salarioBase;
+        this.departamento = departamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(Double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", salarioBase=" + salarioBase +
+                ", departamento=" + departamento +
+                '}';
+    }
 }
