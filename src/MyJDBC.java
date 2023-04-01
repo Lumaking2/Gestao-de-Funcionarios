03fdfcad6c74b5f8bbbda0e4323c19a6eead0f5d
Matheus Augusto <104650578+Lumaking2@users.noreply.github.com>
2023-04-01 12:38:12 -0300

    Commit Inicial

---
+++ src/MyJDBC.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MyJDBC {
+
+    public static void main(String[] args) {
+
+        try {
+                //Faz a conexão com o banco de dados
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aula-jdbc", "root", "Luma300599.");
+
+                //Statement
+                Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from pessoa");
+
+            while(resultSet.next()) {
+                System.out.print(resultSet.getString("idpessoa"));
+                System.out.print(" ");
+                System.out.println(resultSet.getString("primeironome"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+}
